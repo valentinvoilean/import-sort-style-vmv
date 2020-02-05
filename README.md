8ca735814481a17e64f0c15c7de73bb72ff30d7a
Valentin Voilean <vali.voilean@gmail.com>
2020-02-05 10:38:58 +0200

    prioritize React packages, add css, types at the end

--- README.md
+++ README.md
@@ -11,19 +11,45 @@ npm install -save-dev import-sort-style-vmv
 Example of sorting:
 
 ```js
-// Modules from the Node.js "standard" library sorted by name module name.
-import { readFile, writeFile } from 'fs';
-import * as path from 'path';
-// Installed NPM packages, sorted by module name.
-import { ... } from 'a';
-import b from 'b';
+// Absolute modules with side effects (not sorted because order may matter)
+import "a";
+import "c";
+import "b";
+
+// Relative modules with side effects (not sorted because order may matter)
+import "./a";
+import "./c";
+import "./b";
+
+// Modules from the React eco-system (react, prop-types, redux modules) library sorted by name
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+// Modules from the Node.js "standard" library sorted by name
+import {readFile, writeFile} from "fs";
+import * as path from "path";
+
+// Third-party modules sorted by name
+import aa from "aa";
+import bb from "bb";
+import cc from "cc";
+
 // Scoped modules (@-prefixed), sorted by module name.
-import foo from '@a/foo';
-import { bar, ... } from '@b/bar';
-// All absolute imports sorted by module name.
-import x from 'baz/a';
-// All relative imports sorted by dot count, then module name.
-import x from '../../baz/a';
-import z from './baz/b';
-import y from './baz/c';
+import aa from "@a/a";
+import { bb } from "@b/b";
+
+// First-party modules sorted by "relative depth" and then by name
+import aaa from "../../aaa";
+import bbb from "../../bbb";
+import aaaa from "../aaaa";
+import bbbb from "../bbbb";
+import aaaaa from "./aaaaa";
+import bbbbb from "./bbbbb";
+
+// First-party styles modules sorted by "relative depth" and then by name
+import styles from "./Component.scss";
+
+// Types & Interfaces
+import types from "./types"
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,116 @@
-import {IStyleAPI, IStyleItem} from "import-sort-style";
+import { readdirSync } from 'fs';
+
+import { IStyleAPI, IStyleItem } from 'import-sort-style';
+
+const fixedOrder = ['react', 'prop-types'];
 
 export default function(styleApi: IStyleAPI): IStyleItem[] {
-
   const {
     alias,
-    always,
     dotSegmentCount,
     isScopedModule,
     isNodeModule,
     moduleName,
     naturally,
     unicode,
-    isInstalledModule,
+    hasNoMember,
+    and,
+    not,
     isAbsoluteModule,
-    isRelativeModule
+    isRelativeModule,
   } = styleApi;
 
+  const modules = readdirSync('./node_modules');
+
+  const isFromNodeModules = imported => modules.indexOf(imported.moduleName.split('/')[0]) !== -1;
+  const isReactModule = imported => Boolean(imported.moduleName.match(/^(react|prop-types|redux)/));
+  const isStylesModule = imported => Boolean(imported.moduleName.match(/\.(s?css|less)$/));
+  const isTypesModule = imported => Boolean(imported.moduleName.match(/types$/));
+
+  const reactComparator = (name1, name2) => {
+    let i1 = fixedOrder.indexOf(name1);
+    let i2 = fixedOrder.indexOf(name2);
+
+    i1 = i1 === -1 ? Number.MAX_SAFE_INTEGER : i1;
+    i2 = i2 === -1 ? Number.MAX_SAFE_INTEGER : i2;
+
+    return i1 === i2 ? naturally(name1, name2) : i1 - i2;
+  };
+
   return [
-    // Node packages, sorted by module name.
+    // import "foo"
+    { match: and(hasNoMember, isAbsoluteModule) },
+    { separator: true },
+
+    // import "./foo"
+    { match: and(hasNoMember, isRelativeModule, not(isStylesModule), not(isTypesModule)) },
+    { separator: true },
+
+    // import React from "react";
+    {
+      match: isReactModule,
+      sort: moduleName(reactComparator),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import … from "fs";
     {
       match: isNodeModule,
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
-    {separator: true},
-    // Installed NPM packages, sorted by module name.
+    { separator: true },
+
+    // import uniq from 'lodash/uniq';
     {
-      match: isInstalledModule(__filename),
+      match: isFromNodeModules,
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
-    {separator: true},
+    { separator: true },
     // Scoped modules (@-prefixed), sorted by module name.
     {
       match: isScopedModule,
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
-    {separator: true},
-    // Absolute modules sorted by module name.
+    { separator: true },
+    // import Component from "components/Component.jsx";
     {
       match: isAbsoluteModule,
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
-    {separator: true},
-    // All relative imports sorted by dot count, then module name.
+    { separator: true },
+
+    // import … from "./foo";
+    // import … from "../foo";
     {
-      match: isRelativeModule,
+      match: and(isRelativeModule, not(isStylesModule), not(isTypesModule)),
       sort: [dotSegmentCount, moduleName(naturally)],
       sortNamedMembers: alias(unicode),
     },
-    {separator: true},
+    { separator: true },
+
+    // import "./styles.css";
+    { match: and(hasNoMember, isRelativeModule, isStylesModule) },
+
+    // import styles from "./Components.scss";
+    {
+      match: isStylesModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import types from "./types";
+    {
+      match: isTypesModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+    { separator: true },
   ];
 }
